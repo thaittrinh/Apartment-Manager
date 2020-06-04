c180ee18da0b90a1de70ddddb635cba145abf749
tungnd1105 <64002250+tungnd1105@users.noreply.github.com>
2020-06-04 20:42:56 +0700

    add  class entity

---
+++ src/main/java/poly/com/entity/PriceGarbages.java
@@ -0,0 +1,45 @@
+package poly.com.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PriceGarbages implements Serializable{
+// <------------------------------------------ Class Entity PriceGarbages -------------------------------------------------->
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@NotNull
+	private Double price;
+
+	@NotNull
+	@Column(unique = true)	
+	@Temporal(TemporalType.DATE)	 
+  	@DateTimeFormat(pattern = "yyyy-MM-dd") //MM/dd/yyyy
+	private Date date;
+
+	private String note;
+
+}
