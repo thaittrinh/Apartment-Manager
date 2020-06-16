a53d88fb9422fce9e0897760639c5773a03fa6c6
tungnd1105 <64002250+tungnd1105@users.noreply.github.com>
2020-06-16 16:14:41 +0700

    fix

--- src/main/java/poly/com/repository/EmployeeRepository.java
+++ src/main/java/poly/com/repository/EmployeeRepository.java
@@ -3,7 +3,12 @@ package poly.com.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import poly.com.entity.Employee;
+import poly.com.entity.Resident;
+
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+  Optional<Employee> findByIndentitycardAndPhone(String identitycard, String phone);
 
 }
