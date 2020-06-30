8f6e074227c7b3d4bc2a18af4a3d0bde7e99e5ea
Trịnh Thái <trinhtrongthaipy@gmail.com>
2020-06-30 18:50:59 +0700

    commit

--- src/main/java/poly/com/security/UserDetailServiceImpl.java
+++ src/main/java/poly/com/security/UserDetailServiceImpl.java
@@ -1,11 +1,17 @@
 package poly.com.security;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import poly.com.entity.Employee;
+import poly.com.entity.Role;
 import poly.com.repository.EmployeeRepository;
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
@@ -13,8 +19,6 @@ public class UserDetailServiceImpl implements UserDetailsService {
     /* ------------------------------------- Class EmployeeDetailService -------------------------------------*/
     @Autowired
     EmployeeRepository employeeRepository;
-
-    /* ------------------------------------------------*/
 
 
     /*------------- Load employee by username------------------------ */
@@ -26,9 +30,18 @@ public class UserDetailServiceImpl implements UserDetailsService {
         
         if (employee.getRoles().size() == 0)  
             	throw new UsernameNotFoundException(username); 	      
+              
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        Set<Role> roles = employee.getRoles();
         
-        return new UserDetailsImpl(employee);
-
+        for (Role role : roles) {
+            grantedAuthorities.add(new SimpleGrantedAuthority(role.getName()+""));
+        }
+        
+        return new org.springframework.security.core.userdetails.User(
+        		employee.getUsername(), employee.getPassword(), grantedAuthorities);
     }
+
+    
 
 }
