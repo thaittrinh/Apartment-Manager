1e5c99d55c5ed751545b5098f3e106495db82046
Trịnh Thái <trinhtrongthaipy@gmail.com>
2020-08-09 16:28:56 +0700

    fix

--- src/main/java/poly/com/service/ApartmentIndexService.java
+++ src/main/java/poly/com/service/ApartmentIndexService.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import poly.com.constant.ElectricityLimits;
 import poly.com.constant.MessageError;
@@ -39,7 +38,6 @@ import poly.com.request.CreateIndexRequest;
 import poly.com.request.UpdateIndexRequest;
 
 @Service
-@Transactional(rollbackFor = Exception.class)
 public class ApartmentIndexService {
 
 	@Autowired
